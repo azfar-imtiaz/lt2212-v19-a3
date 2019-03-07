e3805d25b342db243c4cab96cfad2642d83c62ad
Azfar Imtiaz <azfar.imtiaz@live.com>
2019-03-07 22:32:06 +0100

    Add support for -T parameter, add various conditions for corner cases, correct POS tag removal regex

--- gendata.py
+++ gendata.py
@@ -11,7 +11,7 @@ import pandas as pd
 # this function removes the POS tags from text, returning only words
 def get_words_from_line(line):
     # regex to remove POS tags or punctuation marks followed by a slash
-    rg = r'\/([A-Z]+|\W)'
+    rg = r'\/[^\s]+'
     line = re.sub(rg, '', line)
     # replace multiple spaces with a single space
     line = re.sub(r'\s+', ' ', line)
@@ -84,13 +84,14 @@ def generate_ngram_model(text_lines, encoded_word_vectors, n=3):
             feature_vector.append(output_label)
             ngram_model.append(feature_vector)
             start += 1
-
+            
     ngram_model = pd.DataFrame(ngram_model)
     return ngram_model
 
 
 # add whatever additional imports you may need here. You may not use the
 # scikit-learn OneHotEncoder, or any related automatic one-hot encoders.
+import random
 
 parser = argparse.ArgumentParser(description="Convert text to features")
 parser.add_argument("-N", "--ngram", metavar="N", dest="ngram", type=int, default=3, help="The length of ngram to be considered (default 3).")
@@ -100,6 +101,9 @@ parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
 parser.add_argument("-E", "--end", metavar="E", dest="endline",
                     type=int, default=None,
                     help="What line of the input data file to end on. Default is None, whatever the last line is.")
+parser.add_argument("-T", "--test-lines", metavar="T", dest="testlines",
+                    type=int, default=5,
+                    help="How many lines in the selected number of lines should be used for testing.")
 parser.add_argument("inputfile", type=str,
                     help="The file name containing the text data.")
 parser.add_argument("outputfile", type=str,
@@ -112,6 +116,15 @@ text_lines = load_text_from_file(args.inputfile)
 
 print("Starting from line {}.".format(args.startline))
 if args.endline:
+    if args.startline < 0 or args.endline < 0:
+        print("Please enter positive numbers for line indices")
+        exit(1)
+    if args.startline >= args.endline:
+        print("Please ensure that the ending line is greater than the starting line")
+        exit(1)
+    if abs(args.startline - args.endline) <= 1:
+        print("Please ensure that the difference between starting and ending line numbers if greater than 1")
+        exit(1)
     print("Ending at line {}.".format(args.endline))
     text_lines = text_lines[args.startline : args.endline]
 else:
@@ -120,16 +133,41 @@ else:
 
 print("Constructing {}-gram model.".format(args.ngram))
 # text_lines = text_lines[:1]
+
 print("Generating vocabulary...")
 vocabulary = get_vocabulary(text_lines)
+
 print("Generating one hot encoded vectors...")
 encoded_word_vectors = generate_one_hot_vectors(vocabulary)
-print("Generating ngram model...")
-ngram_model = generate_ngram_model(text_lines, encoded_word_vectors, n=3)
-print(ngram_model.head())
+
+print("Creating train test split...")
+print("%d lines will be used for testing" % args.testlines)
+if args.testlines > len(text_lines):
+    print("Please ensure that the number of lines selected for start and end is greater than the number of testing lines")
+    exit(1)
+
+# shuffle the data, then use the first -T lines for testing, and the remaining for training
+random.shuffle(text_lines)
+test_lines = text_lines[: args.testlines]
+train_lines = text_lines[args.testlines + 1 : ]
+
+print("Generating ngram model for training data...")
+ngram_model_train = generate_ngram_model(train_lines, encoded_word_vectors, n=args.ngram)
+print(ngram_model_train.head())
+
+print("Generating ngram model for testing data...")
+ngram_model_test = generate_ngram_model(test_lines, encoded_word_vectors, n=args.ngram)
+print(ngram_model_test.head())
 
 print("Writing table to {}.".format(args.outputfile))
-ngram_model.to_csv(args.outputfile)
+if not args.outputfile.lower().endswith("csv"):
+    print("Note: The output will be written in .csv format")
+
+train_filename = "%s_train.csv" % args.outputfile.split('.')[0]
+test_filename = "%s_test.csv" % args.outputfile.split('.')[0]
+
+ngram_model_train.to_csv(train_filename)
+ngram_model_test.to_csv(test_filename)
     
 # THERE ARE SOME CORNER CASES YOU HAVE TO DEAL WITH GIVEN THE INPUT
 # PARAMETERS BY ANALYZING THE POSSIBLE ERROR CONDITIONS.
